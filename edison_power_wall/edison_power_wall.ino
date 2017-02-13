ca3287beacf8ae33275abc0b3d2c3f8f29ed3e5f
Ryan Rose <rytrose@gmail.com>
2017-02-13 10:27:05 -0500

    added experimental audio processing logic

--- edison_power_wall/edison_power_wall.ino
+++ edison_power_wall/edison_power_wall.ino
@@ -15,9 +15,27 @@
 #include <Time.h>
 #include <TimeLib.h>
 
-/*************************
+// Defines voltage threshold for audio playing
+#define THRESH 3000
+
+// Defines number of iterations the audio logic loops
+#define SONG_ITERATIONS 100
+
+// Analog input pins
+#define panel_0 A0
+#define panel_1 A1
+#define panel_2 A2
+#define panel_3 A3
+#define panel_4 A4
+#define panel_5 A5
+#define panel_6 A6
+#define panel_7 A7
+#define panel_8 A8
+#define panel_9 A9
+
+/**************************
  * Data collection globals
- *************************/
+ **************************/
 
 // MAC address of Ethernet 2 Shield
 byte mac[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
@@ -29,23 +47,13 @@ TextFinder finder(client);
 int temp;
 char condition[40];
 
-/*************************
- * Audio processing globals
- *************************/
-
-// Analog input pins
-int panel_0 = A0;
-int panel_1 = A1;
-int panel_2 = A2;
-int panel_3 = A3;
-int panel_4 = A4;
-int panel_5 = A5;
-int panel_6 = A6;
-int panel_7 = A7;
-int panel_8 = A8;
-int panel_9 = A9;
+/**********
+ * Setup
+ **********/
 
 void setup() {
+  // Setup serial
+  Serial.begin(9600);
   
   // PINS DEFAULT TO INPUT
   
@@ -75,16 +83,14 @@ void loop() {
   }
 
   // Read data from Yahoo! Weather
-  if(!(minute() % 3)){
-    // If the client is connected to the Yahoo! server
-    if(client.connected()){
-      finder.find("temp=");
-      temp = finder.getValue();
-      finder.find("<yweather:condition  text=");
-      finder.getString("\"", "\"", condition, 40);
-    }
+  // If the client is connected to the Yahoo! server
+  if(client.connected()){
+    finder.find("temp=");
+    temp = finder.getValue();
+    finder.find("<yweather:condition  text=");
+    finder.getString("\"", "\"", condition, 40);
   }
-
+  
   // Reset client
   client.stop();
   client.flush();
@@ -92,8 +98,26 @@ void loop() {
   /********************
    * Audio processing
    ********************/
+  int thresholds[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  int panels[10] = {panel_0, panel_1, panel_2, panel_3, panel_4, panel_5, panel_6, panel_7, panel_8, panel_9}; 
 
-  
-   
-   
+  // Audio processing loop
+  for(int iterations = 0; iterations < SONG_ITERATIONS; iterations++){
+    for(int i = 0; i < 10; i++){
+      // Calculate new threshold (voltage input) value for panel i
+      thresholds[i] += analogRead(panels[i]);
+      // If exceeds threshold, play sound
+      if(thresholds[i] > THRESH){
+        // Reset threshold measurement
+        thresholds[i] = 0;
+        // Play sound
+        
+        // Delay sound length
+        delay(500); // e.g.
+      }
+      // Else loop
+    }
+  }
 }
+
+
